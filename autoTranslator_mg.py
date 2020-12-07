c0c818de50ccf5b8bdb04300e839896eaa5d1b17
Terry chan <napoler2008@gmail.com>
2020-12-07 15:34:30 +0800

    add 远程mg

---
+++ autoTranslator_mg.py
@@ -0,0 +1,56 @@
+# -*-coding:utf-8-*-
+import os
+
+from elasticsearch import Elasticsearch
+from elasticsearch import helpers
+from tkitTranslator import Translator
+# import tkitJson
+import pymongo
+# 引入安装的库
+import Bert_clear_title
+#模型下载自https://www.kaggle.com/terrychanorg/bertcleartitlemodel
+TClear =Bert_clear_title.Marker(model_path="/mnt/data/dev/model/Bert_clear_title/model/")
+TClear.load_model()
+
+
+
+client = pymongo.MongoClient("localhost", 27017)
+DB = client.hugo
+
+es = Elasticsearch('127.0.0.1:9200')
+index_v="terry-index"
+index_v="scrapy_search-2020-11"
+doc_type_v="items"
+query={"query" : {"match_all" : {}}} 
+scanResp= helpers.scan(client= es, query=query, scroll= "10m", index= index_v , doc_type=doc_type_v , timeout="10m")
+# Tjson=tkitJson.Json("data/data.json")
+items=[]
+for i,resp in enumerate( scanResp):
+    print("\n"*2)
+    qid = resp['_id']
+    # print(resp)
+    # print(resp['_source']['title'])
+    one=TClear.pre(resp['_source']['title'])
+
+    # print(TClear.get_mark_data(one[0]))
+    if len(TClear.get_mark_data(one[0]))==0:
+        items.append(resp['_source']['title'])
+    else:
+        try:
+            T = Translator()
+            print(resp['_source']['title'])
+            title=T.render(TClear.get_mark_data(one[0])["title"][0])
+            content=T.render(resp['_source']['content'])
+            print(title)
+            print(content) 
+            data=[{"title":title,"content":content,"_id":qid,"original":resp}]
+            #添加数据
+            # Tjson.save(data)
+            DB.content_pet.update({'_id':qid},{'$set':data[0]},True)
+        except:
+            print("翻译失败")
+            pass
+
+print("召回失败的：",len(items))      
+print(items)
+
